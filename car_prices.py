8c570eb4267692e94793caff07cd6653537f3025
Sujal Bhandari <116736283+whysujalwhy@users.noreply.github.com>
2023-10-02 12:33:19 +0530

    Update car_prices.py

--- car_prices.py
+++ car_prices.py
@@ -1,10 +1,8 @@
 import sqlite3
 
-# Connect to the SQLite database (or create it if it doesn't exist)
 conn = sqlite3.connect('car_prices.db')
 cursor = conn.cursor()
 
-# Create a table to store car information
 cursor.execute('''
     CREATE TABLE cars (
         id INTEGER PRIMARY KEY,
@@ -14,16 +12,14 @@ cursor.execute('''
     )
 ''')
 
-# Insert some dummy data
 dummy_data = [
-    ('Honda', 'Civic', 18000),
-    ('Toyota', 'Corolla', 19000),
-    ('Ford', 'Focus', 17000),
-    ('Chevrolet', 'Cruze', 16000)
+    ('Swift', 'Dzire', 550000),
+    ('Honda', 'City', 919000),
+    ('Hyundai', 'Creta', 777000),
+    ('Tata', 'Nexon', 886000)
 ]
 
 cursor.executemany('INSERT INTO cars (make, model, price) VALUES (?, ?, ?)', dummy_data)
 
-# Commit changes and close the connection
 conn.commit()
 conn.close()
