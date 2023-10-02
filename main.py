e49e840f5ee8550e1dea7c6a2fd109f5e22f123f
Sujal Bhandari <116736283+whysujalwhy@users.noreply.github.com>
2023-10-02 12:16:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,44 +1,41 @@
+import sqlite3
 import requests
 from bs4 import BeautifulSoup
 from win10toast import ToastNotifier
 import tkinter as tk
 from tkinter import ttk
 from ttkthemes import ThemedStyle
+from tkinter import Tk, Label, Entry, Button, messagebox
 
-# Code for web scraping from cardekho.com 
+
 def check_prices():
-    url = "https://www.cardekho.com/"
-    car_model = "maruti-suzuki-swift" 
-
-    response = requests.get(f"{url}{car_model}.htm")
-    soup = BeautifulSoup(response.content, 'html.parser')
-
-    car_prices = [int(price.text.replace('Rs.', '').replace(',', '').strip()) for price in soup.find_all('div', class_='pricevalue')]
-
+    
+    conn = sqlite3.connect('car_prices.db')
+    cursor = conn.cursor()
+    
     threshold_price = int(threshold_entry.get())
 
-    for price in car_prices:
+    cursor.execute('SELECT * FROM cars')
+    cars = cursor.fetchall()
+
+    for car in cars:
+        car = car[1:]
+        make, model, price = car
         if price < threshold_price:
-            toaster = ToastNotifier()
-            toaster.show_toast("Car Price Alert", f"The price of the {car_model} has dropped below Rs. {threshold_price}!", duration=10)
-            
+            print(f"The price of a {make} {model} has dropped below ${threshold_price}!")
+    conn.close()
+
 # GUI Setup
-root = tk.Tk()
+root = Tk()
 root.title("Car Price Notifier")
 
-style = ThemedStyle(root)
-style.set_theme("plastik")
+label_threshold = Label(root, text="Threshold Price (in USD):")
+label_threshold.pack(pady=5)
 
-frame = ttk.Frame(root, padding="30")
-frame.grid(row=0, column=0, sticky=(tk.W, tk.E, tk.N, tk.S))
+threshold_entry = Entry(root)
+threshold_entry.pack(pady=5)
 
-label = ttk.Label(frame, text="Enter Threshold Price (in INR):")
-label.grid(row=0, column=0, pady=(0, 10), sticky=tk.W)
-
-threshold_entry = ttk.Entry(frame)
-threshold_entry.grid(row=1, column=0, pady=(0, 10))
-
-check_button = ttk.Button(frame, text="Check Prices", command=check_prices)
-check_button.grid(row=2, column=0, pady=(0, 10))
+check_button = Button(root, text="Check Prices", command=check_prices)
+check_button.pack(pady=10)
 
 root.mainloop()
