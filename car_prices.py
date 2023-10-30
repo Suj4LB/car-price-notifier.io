1c8831a0e94ae3dae3b9886f11af883198707d79
pratiksha bahuguna <129508103+Pratiksha-25@users.noreply.github.com>
2023-10-30 20:24:12 +0530

    Update car_prices.py

--- car_prices.py
+++ car_prices.py
@@ -1,8 +1,13 @@
+# Import the sqlite3 module to work with SQLite databases.
 import sqlite3
 
+# Connect to or create a SQLite database file named 'car_prices.db'.
 conn = sqlite3.connect('car_prices.db')
+
+# Create a cursor object to interact with the database.
 cursor = conn.cursor()
 
+# Create a table named 'cars' in the database to store car-related data.
 cursor.execute('''
     CREATE TABLE cars (
         id INTEGER PRIMARY KEY,
@@ -12,6 +17,7 @@ cursor.execute('''
     )
 ''')
 
+# Define dummy data for cars, including make, model, and price.
 dummy_data = [
     ('Swift', 'Dzire', 550000),
     ('Honda', 'City', 919000),
@@ -19,7 +25,11 @@ dummy_data = [
     ('Tata', 'Nexon', 886000)
 ]
 
+# Insert the dummy data into the 'cars' table using a parameterized query.
 cursor.executemany('INSERT INTO cars (make, model, price) VALUES (?, ?, ?)', dummy_data)
 
+# Commit the changes to the database to save the data.
 conn.commit()
+
+# Close the database connection.
 conn.close()
